d8de89ba58b1026550362f167878d51cca52e39a
Omar Ahmed <omar.ahmed9806@gmail.com>
2026-09-06 01:31:54 +0300

    feat: implement auth service with registration and login logic and configure users module with jwt and mongoose integration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,60 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { User } from '../users/users.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { RegisterDto } from './dtos/register.dto';
-import bcrypt from "bcryptjs"
+import bcrypt from 'bcryptjs';
+import { LoginDto } from './dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
+    private readonly jwtService: JwtService
   ) {}
 
   public async register(registerDto: RegisterDto) {
     const { email, password } = registerDto;
     const existedUser = await this.userModel.findOne({ email });
     if (existedUser) throw new BadRequestException('email already exists');
-    
+
     const hashedPassword = await this.hashPassword(password);
 
     const newUser = await this.userModel.create({
-        ...registerDto,
-        password:hashedPassword
+      ...registerDto,
+      password: hashedPassword,
     });
-    return newUser.save()
+    await newUser.save();
+    const token = await this.generateToken(newUser._id.toString(), newUser.email, newUser.role)
+    return {user:newUser, token}
   }
 
-  public async hashPassword(password: string): Promise<string> {
+  public async login(loginDto:LoginDto){
+    const user = await this.userModel.findOne({email:loginDto.email})
+    if(!user){
+      throw new UnauthorizedException("Invalid email or password")
+    }
+
+    const isMatch = await bcrypt.compare(loginDto.password, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const token = await this.generateToken(user._id.toString(), user.email, user.role);
+
+    return {
+      user,
+      token
+    }
+  }
+
+  private async hashPassword(password: string): Promise<string> {
     const salt = await bcrypt.genSalt(10);
     return bcrypt.hash(password, salt);
   }
+
+  private async generateToken(id: string, email: string, role:string): Promise<string> {
+    const payload = { id, email, role };
+    return this.jwtService.signAsync(payload);
+  }
+
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,17 +1,21 @@
-import { forwardRef, Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { User, UserSchema } from "./users.model";
-import { UsersController } from "./users.controller";
-import { UsersService } from "./users.service";
-import { FavoritesProvider } from "./favorites.provider";
-import { AuthModule } from "../auth/auth.module";
-import { JwtModule } from "@nestjs/jwt";
+import { forwardRef, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './users.model';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { FavoritesProvider } from './favorites.provider';
+import { AuthModule } from '../auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    exports:[MongooseModule],
+  exports: [MongooseModule],
 
-    imports:[JwtModule, forwardRef(()=>AuthModule),MongooseModule.forFeature([{name:User.name, schema:UserSchema}])],
-    controllers: [UsersController],
-    providers: [UsersService,FavoritesProvider],
+  imports: [
+    JwtModule,
+    forwardRef(() => AuthModule),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, FavoritesProvider],
 })
-export class UsersModule{}
+export class UsersModule {}
